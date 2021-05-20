5cc65ff4e294840b0d3906ab692ba308190adf57
hak2711 <hak271111@gmail.com>
2021-05-20 16:42:29 +0900

    Delete Exception throw

--- back/_8th_seminar/test-practice/src/main/java/com/efub/seminar/springboot/service/UserService.java
+++ back/_8th_seminar/test-practice/src/main/java/com/efub/seminar/springboot/service/UserService.java
@@ -6,22 +6,19 @@ import com.efub.seminar.springboot.web.dto.UserResponseDto;
 import com.efub.seminar.springboot.web.dto.UserSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
 
-    @Transactional
     public UserResponseDto findById(Long id){
-        User entity = userRepository.findById(id).orElseThrow(() -> new
-                IllegalArgumentException("해당 사용자가 없습니다. id = "+id));
-
+//        User entity = userRepository.findById(id).orElseThrow(() -> new
+//                IllegalArgumentException("해당 사용자가 없습니다. id = "+id));
+        User entity = userRepository.getOne(id);
         return new UserResponseDto(entity);
     }
 
-    @Transactional
     public User save(UserSaveRequestDto requestDto){
         return userRepository.save(requestDto.toEntity());
     }
