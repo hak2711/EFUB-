8d63ac4871fe8f0ba5ec8652bd2ba1e77d8e8c7b
hak2711 <hak271111@gmail.com>
2021-05-20 20:11:10 +0900

    Delete annotation

--- back/_8th_seminar/test-practice/src/main/java/com/efub/seminar/springboot/domain/UserRepository.java
+++ back/_8th_seminar/test-practice/src/main/java/com/efub/seminar/springboot/domain/UserRepository.java
@@ -1,8 +1,6 @@
 package com.efub.seminar.springboot.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
